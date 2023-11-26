1031aef979162084db0bd458f19968275f30edb5
DalyChouikh <chouikhdaly215@gmail.com>
2023-11-26 20:22:05 +0100

    Added createTask method and its business logic

--- src/main/java/dev/daly/todolist/services/TaskService.java
+++ src/main/java/dev/daly/todolist/services/TaskService.java
@@ -1,15 +1,19 @@
 package dev.daly.todolist.services;
 
+import dev.daly.todolist.dto.TaskRequest;
 import dev.daly.todolist.models.Status;
 import dev.daly.todolist.models.Task;
 import dev.daly.todolist.repository.TaskRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @Service
@@ -25,15 +29,42 @@ public class TaskService {
     public Task getTaskById(Long id){
         return taskRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Task with ID " + id + " not found"));
     }
-
     public List<Task> getTasksByTitle(String keyword){
         return  taskRepository.findTasksByTitleContains(keyword);
+    }
+
+    public ResponseEntity<String> createTask(TaskRequest taskRequest){
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd");
+            LocalDate parsedDate = LocalDate.parse(taskRequest.getDueDate());
+            if(taskRequest.getTitle() == null || taskRequest.getTitle().isEmpty()){
+                return new ResponseEntity<>("Title cannot be empty", HttpStatus.BAD_REQUEST);
+            }
+            else if(parsedDate.isBefore(LocalDate.now())){
+                return new ResponseEntity<>("Due date cannot be in the past", HttpStatus.BAD_REQUEST);
+            }
+            else if (taskRequest.getStatus() == Status.DONE && parsedDate.isAfter(LocalDate.now())){
+                return new ResponseEntity<>("Due date cannot be in the future if status is done", HttpStatus.BAD_REQUEST);
+            }
+            else if (taskRequest.getStatus() == Status.IN_PROGRESS && parsedDate.isBefore(LocalDate.now())){
+                return new ResponseEntity<>("Due date cannot be in the past if status is in progress", HttpStatus.BAD_REQUEST);
+            }
+            Task task = Task.builder()
+                    .title(taskRequest.getTitle())
+                    .description(taskRequest.getDescription())
+                    .status(taskRequest.getStatus())
+                    .dueDate(parsedDate)
+                    .build();
+            taskRepository.save(task);
+            return new ResponseEntity<>("Task created successfully", HttpStatus.CREATED);
+        } catch (DateTimeParseException e) {
+            return new ResponseEntity<>("Invalid date format. Expected format: yy-MM-dd", HttpStatus.BAD_REQUEST);
+        }
     }
 
     public Task createOrUpdateTask(Task task){
         return taskRepository.save(task);
     }
-
     public void deleteTask(Long taskId) {
         taskRepository.deleteById(taskId);
     }
