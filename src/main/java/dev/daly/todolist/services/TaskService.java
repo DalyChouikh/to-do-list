564fad8ab6d63df0d41b016b8abce4a7c7c49fc7
DalyChouikh <chouikhdaly215@gmail.com>
2023-11-26 22:23:39 +0100

    Extracted the validation of a task to a method

--- src/main/java/dev/daly/todolist/services/TaskService.java
+++ src/main/java/dev/daly/todolist/services/TaskService.java
@@ -40,46 +40,22 @@ public class TaskService {
     }
 
     public ResponseEntity<String> createTask(TaskRequest taskRequest){
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd");
-            LocalDate parsedDate = LocalDate.parse(taskRequest.getDueDate());
-            if(taskRequest.getTitle() == null || taskRequest.getTitle().isEmpty()){
-                return new ResponseEntity<>("Title cannot be empty", HttpStatus.BAD_REQUEST);
-            }
-            else if(taskRequest.getDueDate() == null || taskRequest.getDueDate().isEmpty()){
-                return new ResponseEntity<>("Due date cannot be empty", HttpStatus.BAD_REQUEST);
-            }
-            else if(taskRequest.getStatus() == null){
-                return new ResponseEntity<>("Status cannot be empty", HttpStatus.BAD_REQUEST);
-            } else if (taskRequest.getStatus() != Status.IN_PROGRESS && taskRequest.getStatus() != Status.DONE) {
-                return new ResponseEntity<>("Status must be either In Progress or Done", HttpStatus.BAD_REQUEST);
-            } else if(parsedDate.isBefore(LocalDate.now())){
-                return new ResponseEntity<>("Due date cannot be in the past", HttpStatus.BAD_REQUEST);
-            }
-            else if (taskRequest.getStatus() == Status.DONE && parsedDate.isAfter(LocalDate.now())){
-                return new ResponseEntity<>("Due date cannot be in the future if status is done", HttpStatus.BAD_REQUEST);
-            }
-            else if (taskRequest.getStatus() == Status.IN_PROGRESS && parsedDate.isBefore(LocalDate.now())){
-                return new ResponseEntity<>("Due date cannot be in the past if status is in progress", HttpStatus.BAD_REQUEST);
-            }else if(taskRepository.existsByTitle(taskRequest.getTitle())){
-                return new ResponseEntity<>("Task with title " + taskRequest.getTitle() + " already exists", HttpStatus.BAD_REQUEST);
-            }
-            Task task = Task.builder()
-                    .title(taskRequest.getTitle())
-                    .description(taskRequest.getDescription())
-                    .status(taskRequest.getStatus())
-                    .dueDate(parsedDate)
-                    .build();
-            taskRepository.save(task);
-            return new ResponseEntity<>("Task created successfully", HttpStatus.CREATED);
-        } catch (DateTimeParseException e) {
-            return new ResponseEntity<>("Invalid date format. Expected format: yy-MM-dd", HttpStatus.BAD_REQUEST);
+        ResponseEntity<String> validationResponse = validateTaskRequest(taskRequest);
+        if(validationResponse != null){
+            return validationResponse;
+        } else if(taskRepository.existsByTitle(taskRequest.getTitle())){
+            return new ResponseEntity<>("Task with title " + taskRequest.getTitle() + " already exists", HttpStatus.BAD_REQUEST);
         }
+        Task task = Task.builder()
+                .title(taskRequest.getTitle())
+                .description(taskRequest.getDescription())
+                .status(taskRequest.getStatus())
+                .dueDate(LocalDate.parse(taskRequest.getDueDate()))
+                .build();
+        taskRepository.save(task);
+        return new ResponseEntity<>("Task created successfully", HttpStatus.CREATED);
     }
 
-    public Task createOrUpdateTask(Task task){
-        return taskRepository.save(task);
-    }
     public ResponseEntity<String> deleteTask(Long taskId) {
         if(!taskRepository.existsById(taskId)){
             return new ResponseEntity<>("Task with ID " + taskId + " doesn't exist", HttpStatus.NOT_FOUND);
@@ -90,39 +66,50 @@ public class TaskService {
 
     public ResponseEntity<String> updateTask(Long taskId, TaskRequest taskRequest) {
         Optional<Task> task = taskRepository.findById(taskId);
+        if(task.isEmpty()){
+            return new ResponseEntity<>("Task with ID " + taskId + " doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        ResponseEntity<String> validationResponse = validateTaskRequest(taskRequest);
+        if(validationResponse != null){
+            return validationResponse;
+        }else if(taskRepository.existsByTitle(taskRequest.getTitle()) && !task.get().getTitle().equals(taskRequest.getTitle())){
+            return new ResponseEntity<>("Task with title " + taskRequest.getTitle() + " already exists", HttpStatus.BAD_REQUEST);
+        }
+        task.get().setTitle(taskRequest.getTitle());
+        task.get().setDescription(taskRequest.getDescription());
+        task.get().setStatus(taskRequest.getStatus());
+        task.get().setDueDate(LocalDate.parse(taskRequest.getDueDate()));
+        taskRepository.save(task.get());
+        return new ResponseEntity<>("Task with ID " + taskId + " updated successfully", HttpStatus.OK);
+    }
+
+    private ResponseEntity<String> validateTaskRequest(TaskRequest taskRequest) {
+        LocalDate parsedDate;
         try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd");
-            LocalDate parsedDate = LocalDate.parse(taskRequest.getDueDate());
-            if(task.isEmpty()){
-                return new ResponseEntity<>("Task with ID " + taskId + " doesn't exist", HttpStatus.NOT_FOUND);
-            }
-            else if(taskRequest.getTitle() == null || taskRequest.getTitle().isEmpty()){
-                return new ResponseEntity<>("Title cannot be empty", HttpStatus.BAD_REQUEST);
-            }
-            else if(taskRequest.getDueDate() == null || taskRequest.getDueDate().isEmpty()){
-                return new ResponseEntity<>("Due date cannot be empty", HttpStatus.BAD_REQUEST);
-            }
-            else if(taskRequest.getStatus() == null){
-                return new ResponseEntity<>("Status cannot be empty", HttpStatus.BAD_REQUEST);
-            }
-            else if(taskRequest.getStatus() == Status.DONE && parsedDate.isAfter(LocalDate.now())){
-                return new ResponseEntity<>("Due date cannot be in the future if status is done", HttpStatus.BAD_REQUEST);
-            }
-            else if(taskRequest.getStatus() == Status.IN_PROGRESS && parsedDate.isBefore(LocalDate.now())){
-                return new ResponseEntity<>("Due date cannot be in the past if status is in progress", HttpStatus.BAD_REQUEST);
-            }else if(taskRepository.existsByTitle(taskRequest.getTitle()) && !task.get().getTitle().equals(taskRequest.getTitle())){
-                return new ResponseEntity<>("Task with title " + taskRequest.getTitle() + " already exists", HttpStatus.BAD_REQUEST);
-            }
-            else{
-                task.get().setTitle(taskRequest.getTitle());
-                task.get().setDescription(taskRequest.getDescription());
-                task.get().setStatus(taskRequest.getStatus());
-                task.get().setDueDate(parsedDate);
-                taskRepository.save(task.get());
-                return new ResponseEntity<>("Task with ID " + taskId + " updated successfully", HttpStatus.OK);
-            }
+            parsedDate = LocalDate.parse(taskRequest.getDueDate());
         } catch (DateTimeParseException e) {
             return new ResponseEntity<>("Invalid date format. Expected format: yy-MM-dd", HttpStatus.BAD_REQUEST);
         }
+
+        if(taskRequest.getTitle() == null || taskRequest.getTitle().isEmpty()){
+            return new ResponseEntity<>("Title cannot be empty", HttpStatus.BAD_REQUEST);
+        }
+        else if(taskRequest.getDueDate() == null || taskRequest.getDueDate().isEmpty()){
+            return new ResponseEntity<>("Due date cannot be empty", HttpStatus.BAD_REQUEST);
+        }
+        else if(taskRequest.getStatus() == null){
+            return new ResponseEntity<>("Status cannot be empty", HttpStatus.BAD_REQUEST);
+        } else if (taskRequest.getStatus() != Status.IN_PROGRESS && taskRequest.getStatus() != Status.DONE) {
+            return new ResponseEntity<>("Status must be either In Progress or Done", HttpStatus.BAD_REQUEST);
+        } else if(parsedDate.isBefore(LocalDate.now())){
+            return new ResponseEntity<>("Due date cannot be in the past", HttpStatus.BAD_REQUEST);
+        }
+        else if (taskRequest.getStatus() == Status.DONE && parsedDate.isAfter(LocalDate.now())){
+            return new ResponseEntity<>("Due date cannot be in the future if status is done", HttpStatus.BAD_REQUEST);
+        }
+        else if (taskRequest.getStatus() == Status.IN_PROGRESS && parsedDate.isBefore(LocalDate.now())){
+            return new ResponseEntity<>("Due date cannot be in the past if status is in progress", HttpStatus.BAD_REQUEST);
+        }
+        return null;
     }
 }
