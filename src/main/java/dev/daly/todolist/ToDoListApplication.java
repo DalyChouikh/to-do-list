1026b7acb587f8ea189ac7b9b23d3b7e6bfdadab
DalyChouikh <chouikhdaly215@gmail.com>
2023-11-26 19:41:20 +0100

    Added CrossOrgin annotation

--- src/main/java/dev/daly/todolist/ToDoListApplication.java
+++ src/main/java/dev/daly/todolist/ToDoListApplication.java
@@ -2,8 +2,10 @@ package dev.daly.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class ToDoListApplication {
 
     public static void main(String[] args) {
