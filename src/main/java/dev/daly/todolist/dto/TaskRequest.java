75a81dfda39bf82e3d5d4979a9eefb28846ecd60
DalyChouikh <chouikhdaly215@gmail.com>
2023-11-26 19:46:29 +0100

    Added the TaskRequest

--- src/main/java/dev/daly/todolist/dto/TaskRequest.java
+++ src/main/java/dev/daly/todolist/dto/TaskRequest.java
@@ -1,2 +1,21 @@
-package dev.daly.todolist.dto;public class TaskRequest {
+package dev.daly.todolist.dto;
+
+import dev.daly.todolist.models.Status;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskRequest {
+    @NonNull
+    private String title;
+    private String description;
+    @NonNull
+    private Status status;
+    @NonNull
+    private LocalDate dueDate;
 }
