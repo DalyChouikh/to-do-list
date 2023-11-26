a9c747939c381196e683f1151da8b1b685cf8344
DalyChouikh <chouikhdaly215@gmail.com>
2023-11-26 19:42:21 +0100

    Added the enum types TODO, IN_PROGRESS, DONE in status

--- src/main/java/dev/daly/todolist/models/Status.java
+++ src/main/java/dev/daly/todolist/models/Status.java
@@ -1,10 +1,37 @@
 package dev.daly.todolist.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+@Getter
 public enum Status {
-    PENDING("PENDING"),
-    DONE("DONE");
+    TODO("To Do"),
+    IN_PROGRESS("In Progress"),
+    DONE("Done");
 
+    private final String status;
 
-    Status(String done) {
+    Status(String s) {
+        this.status = s;
     }
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public static Status fromStatus(String value) {
+        for (Status status : Status.values()) {
+            if (status.status.equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown enum type " + value + ", Allowed values are " + allowedValues());
+    }
+
+    private static String allowedValues() {
+        StringBuilder allowedValues = new StringBuilder();
+        for (Status status : Status.values()) {
+            allowedValues.append(status.status).append(", ");
+        }
+        return allowedValues.substring(0, allowedValues.length() - 2);
+    }
+
+
 }
