748f2cefb5917b6d75f728ffb2779f2ec36f469f
DalyChouikh <chouikhdaly215@gmail.com>
2023-11-27 18:18:31 +0100

    Added TaskResponse class

--- src/main/java/dev/daly/todolist/dto/TaskResponse.java
+++ src/main/java/dev/daly/todolist/dto/TaskResponse.java
@@ -1,4 +1,24 @@
 package dev.daly.todolist.dto;
 
+import dev.daly.todolist.models.Status;
+import lombok.NonNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TaskResponse {
+    @NonNull
+    private String title;
+    private String description;
+    @NonNull
+    private Status status;
+    @NonNull
+    private String dueDate;
 }
