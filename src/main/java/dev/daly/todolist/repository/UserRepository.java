8e7039b7ad1f781495c9f0a01a57356ab656c3d6
DalyChouikh <chouikhdaly215@gmail.com>
2023-11-28 20:04:05 +0100

    Added a user repository

--- src/main/java/dev/daly/todolist/repository/UserRepository.java
+++ src/main/java/dev/daly/todolist/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package dev.daly.todolist.repository;public interface UserRepository {
+package dev.daly.todolist.repository;
+
+import dev.daly.todolist.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
